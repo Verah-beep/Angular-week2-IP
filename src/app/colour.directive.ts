bb780cfea0ac2c39d63054fc0f04a7746564675e
verah-beep <veronikauma75@gmail.com>
2020-08-31 12:07:54 +0300

    add a hostlistener

--- src/app/colour.directive.ts
+++ src/app/colour.directive.ts
@@ -1,2 +1,20 @@
-export class Colour.Directive {
+import { Directive,ElementRef,HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appColour]'
+})
+export class ColourDirective {
+ constructor(private elem:ElementRef){}
+  @HostListener("click") onClicks(){
+   this.textDeco("underline overline")
+ }
+
+ @HostListener("dblclick") onDoubleClicks(){
+   this.textDeco("None")
+ }
+
+ private textDeco(action:string){
+   this.elem.nativeElement.style.textDecoration=action;
+  }
+
 }
