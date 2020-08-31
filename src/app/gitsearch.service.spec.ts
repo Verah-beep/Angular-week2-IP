365c3316dd6bddfc2041767d1b8972f37a748507
verah-beep <veronikauma75@gmail.com>
2020-08-31 11:52:23 +0300

    import an injectable in service

---
+++ src/app/gitsearch.service.spec.ts
@@ -0,0 +1,7 @@
+import { Gitsearch.Service } from './gitsearch.service';
+
+describe('Gitsearch.Service', () => {
+  it('should create an instance', () => {
+    expect(new Gitsearch.Service()).toBeTruthy();
+  });
+});
